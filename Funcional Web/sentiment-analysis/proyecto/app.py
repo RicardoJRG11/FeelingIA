10dff172d7ae9fc6e18349889cb52019b2e1e047
Ricardo José Ruiz Gaviria <rruiz1109@gmail.com>
2023-05-29 00:39:50 -0500

    Proyecto Feeling - Analisis de Sentimientos

---
+++ Funcional Web/sentiment-analysis/proyecto/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+nltk.download('vader_lexicon')
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def main():
+    if request.method == "POST":
+        inp = request.form.get("inp")
+        sid = SentimentIntensityAnalyzer()
+        score = sid.polarity_scores(inp)
+        if score["neg"] != 0:
+            return render_template('index.html', message="Negative😟😥🤬")
+        else:
+            return render_template('index.html', message="Positivo🙂😉😃")
+    return render_template('index.html')
